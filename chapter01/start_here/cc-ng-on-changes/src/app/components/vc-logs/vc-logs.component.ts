61eee2617c4d37ba17dfa46ebdcd421bdd4eb74a
Inthava <coci8750@gmail.com>
2024-03-11 07:17:07 +0000

    read time

--- chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/start_here/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss']
 })
-export class VcLogsComponent implements OnInit {
+export class VcLogsComponent implements OnInit, OnChanges {
+  logs: string[] = [];
   @Input() vName;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    const currValue = changes.vName.currentValue;
+    if (changes.vName.firstChange) {
+      this.logs.push(`initial version is ${currValue.trim()}`)
+    } else {
+      this.logs.push(`version changed to ${currValue.trim()}`)
+      }
   }
 
 }
--- chapter02/start_here/ng-read-time-directive/src/app/app.component.ts
+++ chapter02/start_here/ng-read-time-directive/src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  readTime = "";
+  onReadTimeCalculated(timeStr: string) {
+    this.readTime = timeStr;
+  }
 }
