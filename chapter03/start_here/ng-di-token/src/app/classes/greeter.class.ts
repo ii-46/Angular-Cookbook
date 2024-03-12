f50d4db4f4ac60ae985bfb934798b0de0b94c8f5
Inthava <coci8750@gmail.com>
2024-03-12 13:33:45 +0000

    Configuration injector with a DI InjectionToken

--- chapter03/start_here/ng-di-token/src/app/classes/greeter.class.ts
+++ chapter03/start_here/ng-di-token/src/app/classes/greeter.class.ts
@@ -1,3 +1,4 @@
+import {InjectionToken} from '@angular/core'
 import { User } from '../interfaces/user.interface';
 export class Greeter implements User {
   firstName;
@@ -10,5 +11,9 @@ export class Greeter implements User {
   greet() {
     return `Hello ${this.firstName} ${this.lastName}`;
   }
-
 }
+
+export const GREETER = new InjectionToken("Greeter", {
+  providedIn: "root",
+  factory: ()=> Greeter
+})
